35e04a409d89f72b40c4c17ea1f00e473e1728b7
Marco Damaceno <maadamaceno@gmail.com>
2015-02-25 17:33:13 -0300

    First commit

---
+++ app/controllers/admin/pages_controller.rb
@@ -0,0 +1,64 @@
+module Admin
+  class PageController < ApplicationController
+    before_action :set_page, only: [:show, :edit, :update, :destroy]
+
+    def index
+
+    end
+
+    def show
+    end
+
+    def new
+      @page = Page.new
+    end
+
+    def edit
+    end
+
+    def create
+      @page = Page.new(page_params)
+
+      respond_to do |format|
+        if @page.save
+          format.html { redirect_to [:admin, @page], notice: "Página #{@page.title} criada com sucesso." }
+          format.json { render :show, status: :created, location: @page }
+        else
+          format.html { render :new }
+          format.json { render json: @page.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def update
+      respond_to do |format|
+        if @page.update(page_params)
+          format.html { redirect_to [:admin, @page], notice: 'Página atualizada com sucesso.' }
+          format.json { render :show, status: :ok, location: @page }
+        else
+          format.html { render :edit }
+          format.json { render json: @page.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @page.destroy
+      respond_to do |format|
+        format.html { redirect_to admin_pages_url, notice: 'Album was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+
+    private
+
+    def set_page
+      @page = Album.find(params[:id])
+    end
+
+    def page_params
+      params.require(:page).permit(:title, :slug, :body)
+    end
+
+  end
+end
