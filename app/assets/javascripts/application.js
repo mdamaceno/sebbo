a6d006ce618024406c764db8b81882228086d743
Marco Damaceno <maadamaceno@gmail.com>
2015-03-02 20:46:35 -0300

    Adicionado gem angular-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require angular
 //= require ckeditor-jquery
 //= require_tree .
--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   class PagesController < ApplicationController
     before_action :set_page, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!
 
     def index
       @pages = Page.all
