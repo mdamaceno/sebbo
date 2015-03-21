7a3655c63a37039d0fc4f16123db25f941179e3a
Marco Damaceno <maadamaceno@gmail.com>
2015-03-21 17:07:09 -0300

    Filtro de anúncio na home

--- app/assets/javascripts/controllers/client/products.js
+++ app/assets/javascripts/controllers/client/products.js
@@ -2,7 +2,7 @@ appClient.controller('indexClientProductCtrl', function($scope, $http, $location
 
   $scope.products = [];
 
-  $http.get('/api/products.json')
+  $http.get('/products.json')
     .success(function(data){
       $scope.products = data.products;
     });
