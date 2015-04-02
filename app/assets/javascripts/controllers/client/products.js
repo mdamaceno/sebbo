fbf9cec9c92bce0219fe895ae89972db409281bf
Marco Damaceno <maadamaceno@gmail.com>
2015-04-02 01:58:09 -0300

    Carrinho

--- app/assets/javascripts/controllers/client/products.js
+++ app/assets/javascripts/controllers/client/products.js
@@ -1,3 +1,13 @@
+Array.prototype.unique = function()
+{
+    var tmp = {}, out = [];
+    for(var i = 0, n = this.length; i < n; ++i)
+    {
+        if(!tmp[this[i]]) { tmp[this[i]] = true; out.push(this[i]); }
+    }
+    return out;
+}
+
 appClient.controller('indexClientProductCtrl', function($scope, $http) {
 
   $http.get('/products.json')
@@ -19,11 +29,28 @@ appClient.controller('showClientProductCtrl', function($scope, $http) {
       $scope.product = data.product;
       $scope.product.description = data.product.description;
       console.log($scope.product);
+      localStorage.setItem('dados', 'alguma coisa');
     });
 
-    $scope.rating = 5;
+  $scope.rating = 5;
 
-    // $scope.saveRatingToServer = function(rating) {
-    //   $window.alert('Rating selected - ' + rating);
-    // };
+  $scope.addToCart = function(id, name, qtd) {
+    console.log(id, name, qtd);
+
+    var oldItems = JSON.parse(localStorage.getItem('itemsArray')) || [];
+
+    var newItem = {
+      'id': id,
+      'name': name,
+      'quantity': qtd
+    };
+
+    oldItems.push(newItem);
+    b = oldItems.unique();
+
+    alert(b);
+
+    localStorage.setItem('itemsArray', JSON.stringify(b));
+  }
+
 });
